af8080c109be141e7a16a4a575f6976ff1722007
eddie-lee101 <eddie.lee@asleep.ai>
2023-12-08 14:10:24 +0900

    Release version 1.1.6

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AsleepSDK",
-            url: "https://github.com/asleep-ai/asleep-sdk-ios/releases/download/2.2.0/AsleepSDK.xcframework.zip",
-            checksum: "76c4066bc6c719749574627a8d0d82dd0ef4408c88a7e5b151d3f2894dcd9b84"
+            url: "https://github.com/asleep-ai/asleep-sdk-ios/releases/download/1.1.6/AsleepSDK.xcframework.zip",
+            checksum: "902535f3f9836d0fd5ad80fa64a943c4518c7babc1d845dd303df3478e4fe45f"
         )
     ]
 )
