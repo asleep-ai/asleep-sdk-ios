bf1679ed8b56e878d31d0805ddc59fc942f571a5
Woo <dormire@asleep.ai>
2024-08-14 15:47:30 +0900

    Release version 2.4.8

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AsleepSDK",
-            url: "https://github.com/asleep-ai/asleep-sdk-ios/releases/download/2.4.7/AsleepSDK.xcframework.zip",
-            checksum: "fe69f3afac20895ce760cc5506bb75b07c60735b9a0718cef9a3084c07bddec8"
+            url: "https://github.com/asleep-ai/asleep-sdk-ios/releases/download/2.4.8/AsleepSDK.xcframework.zip",
+            checksum: "e05e3c24ac5352d4325994d4dc96d1565b978e1e2fb8fe0697d09530d4169151"
         )
     ]
 )
