8d2f1d8c7aa354db13bdbec506f15faae1166171
luelh <luelh@asleep.ai>
2026-03-04 15:16:36 +0900

    Release version 3.2.0

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .binaryTarget(
             name: "AsleepSDK",
             url: "https://github.com/asleep-ai/asleep-sdk-ios/releases/download/3.2.0/AsleepSDK.xcframework.zip",
-            checksum: "d292a1ae2440ea6af031d489edd7c8e10444c3a69b861042b5288c2285e67cf2"
+            checksum: "091d6ea0c177f6cbaa40c877d1a5e26f8ae013b1baccac5a0be256bfb5506a5f"
         )
     ]
 )
